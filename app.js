b630246719e309fd31a40bc3ffc9a63c2a8f840f
azizamari <azizamari2003@gmail.com>
2020-08-24 14:35:23 +0100

    json

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ class Products{
     try {
       let result = await fetch('products.json');
       let data = await result.json();
-      return data;
+      let products= data.items;
+      products=products.map(item =>{
+        const {title,price} = item.fields;
+        const {id} = item.sys;
+        const {image} = item.fields.image.fields.file;
+        return {title,price,id,image};
+      })
+      return products
     }
     catch(error){
       console.log(error)
@@ -29,7 +36,25 @@ class Products{
 
 //display products
 class UI{
-
+  displayProducts(products){
+    let result='';
+    products.forEach(product => {
+      result +=`
+      <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to bag
+                    </button>
+                </div>
+                <h3>' '${product.name}</h3>
+                <h4>${"$"+product.price}</h3>
+            </article>
+      `
+    });
+    productsDOM.innerHTML=result;
+  }
 }
 
 //local storage
@@ -42,5 +67,5 @@ document.addEventListener('DOMContentLoaded',()=>{
   const products=new Products();
 
   //get all products
-  products.getProducts().then(data =>console.log(data));
-})
+  products.getProducts().then(products =>ui.displayProducts(products));
+})  
